9f74dd2402a5e1c5d42ffd0afdf789fc3ac9555c
Luca Schirmer <66419654+LucaSchirmer@users.noreply.github.com>
2024-03-17 19:37:29 +0100

    add IconOption to review.js; init landiing Page
    maybe refactor landing page => doesnt need own folder ?
    - maybe consider creating new CSS class that combine tailwind classes like - headline - paragraph - subheading - Quelle - info - Fact ........

--- custom-website-creater/src/app/componetns/customer_reviews/review.js
+++ custom-website-creater/src/app/componetns/customer_reviews/review.js
@@ -1,12 +1,24 @@
 "use-client"
-import React, { useState, useEffect } from 'react';
-import { BsArrowLeft, BsArrowRight, BsX } from 'react-icons/bs';
-
+import React from 'react'
+import Image from 'next/image';
 
 const Review = (props) => {
 
+
+    handleShowIcon = () => {
+        return <Image
+                width={50}
+                height={50}
+                src={"https://google.com/placeholder_img.png"}
+                alt={props.name + " icon"}
+                className="w-full h-[80vh] object-contain border border-white rounded"
+              />;
+    }
+
+
     return (
         <div className='min-w-60 max-w-full w-40 '>
+            {props.hasIcon === true ? handleShowIcon() : "<!-- no Icon -->"}
             <h3> {props.name} </h3>
             <p> {props.text} </p>
         </div>
---
+++ custom-website-creater/src/app/componetns/landing_section/landing_section.js
@@ -0,0 +1,23 @@
+"use-client"
+import React from 'react';
+
+const Landing = (props) => {
+
+    
+
+    return (
+        <main>
+            <div className="landingHeadline">
+                <h2 className="font-semibold">
+                    {props.headline}
+                </h2>
+                <p>
+                    {props.aboutText}
+                </p>
+            </div>
+        </main>
+    )
+
+}
+
+export default Landing;
