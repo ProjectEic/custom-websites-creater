cb1b1877996434f8cd1de5220123a446390fb49b
Luca Schirmer <66419654+LucaSchirmer@users.noreply.github.com>
2024-04-16 20:35:37 +0200

    update navSlide

--- custom-website-creater/src/app/componetns/header.js
+++ custom-website-creater/src/app/componetns/header.js
@@ -6,6 +6,23 @@ const Header = ({ elements }) => {
     // make this perhaps a paramter
     const positionLastElement = elements.length -1;
 
+
+    const handleClickNavSlide = () => {
+        const listItems = document.querySelectorAll('.navSlide li');
+        const navSlide = document.querySelector(".navSlide");
+        navSlide?.classList.toggle("toggleDisplayNoneNavSlide");
+        
+        let navSlideList = Array.prototype.filter.call(listItems, f => !f.classList.contains('toggleDisplay'));  
+
+        navSlideList.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinksFade 500ms ease forwards ${index / 9 + 0.5}s`;
+            }
+        });
+    }
+
     return (
         <header className="text-[var(--onMain-color)] h-[8vh]">
             <nav className="container fixed z-10 max-w-none">
@@ -17,6 +34,18 @@ const Header = ({ elements }) => {
                             className="text-[var(--onMain-color)] hover:text-gray-300 hover:underline transition duration-300 font-bold text-2xl"
                         >
                             {element.title}
+                        </a>
+                    </li>
+                    ))}
+                </ul>
+                <ul className="flex flex-col items-center toggleDisplayNoneNavSlide navSlide">
+                    {elements.slice(1, elements.length - 1).map((element, index) => (
+                    <li className="text-center" key={index}>
+                        <a
+                        href={element.href}
+                        className="text-[var(--onMain-color)] hover:text-gray-300 hover:underline transition duration-300 font-bold text-2xl"
+                        >
+                        {element.title}
                         </a>
                     </li>
                     ))}
@@ -36,45 +65,25 @@ const Header = ({ elements }) => {
                         {elements[positionLastElement].title}
                     </a>
                     </div>
-                    <div class="burgerIcon w-[10%] justify-end">
+                    <div className="burgerIcon w-[10%] justify-end" onClick={handleClickNavSlide}>
                         <div></div>
                         <div></div>
                         <div></div>
                     </div>
-                    <ul className="flex flex-col items-center toggleDisplayNone">
-                        {elements.slice(1, elements.length - 1).map((element, index) => (
-                        <li className="text-center" key={index}>
-                            <a
-                            href={element.href}
-                            className="text-[var(--onMain-color)] hover:text-gray-300 hover:underline transition duration-300 font-bold text-2xl"
-                            >
-                            {element.title}
-                            </a>
-                        </li>
-                        ))}
-                    </ul>
                 </div>
                 <div className="responsiveMobileHeader">
                     <a
                         href={elements[0].href}
-                        className="text-[var(--onMain-color)] hover:text-gray-300 hover:underline transition duration-300 font-bold text-2xl"
+                        className="text-[var(--onMain-color)] hover:text-gray-300 hover:underline w-[50%] transition duration-300 font-bold text-2xl"
                     >
                         {elements[0].title}
                     </a>
-                    <ul className="flex flex-col items-center">
-                        {elements.slice(1).map((element, index) => (
-                        <li className="text-center" key={index}>
-                            <a
-                            href={element.href}
-                            className="text-[var(--onMain-color)] hover:text-gray-300 hover:underline transition duration-300 font-bold text-2xl"
-                            >
-                            {element.title}
-                            </a>
-                        </li>
-                        ))}
-                    </ul>
+                    <div className="burgerIcon w-[50%] justify-end" onClick={handleClickNavSlide}>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </div>
-
             </nav>
         </header>
     );
