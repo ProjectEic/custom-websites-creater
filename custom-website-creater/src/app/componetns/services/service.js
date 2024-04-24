c484436fea384dbd724613052be8996a752b7e9d
Luca Schirmer <66419654+LucaSchirmer@users.noreply.github.com>
2024-04-24 18:39:23 +0200

    update lightbox animation

--- custom-website-creater/src/app/componetns/services/service.js
+++ custom-website-creater/src/app/componetns/services/service.js
@@ -1,12 +1,11 @@
-'use client';
 import React, { useState, useRef } from 'react';
 import Image from 'next/image';
 import { BsX } from 'react-icons/bs';
-import {motion, useInView} from "framer-motion"
+import { motion, useInView, useAnimation } from "framer-motion";
 
 const Service = (service) => {
-    
-  const [lightboxOpen, setLightboxOpen] = useState(false);
+
+    const [lightboxOpen, setLightboxOpen] = useState(false);
 
     const handleShowIcon = (name) => {
         return <Image
@@ -18,7 +17,8 @@ const Service = (service) => {
         />;
     }
 
-    const toggleLightbox = () => {
+    const toggleLightbox = (e) => {
+        e.stopPropagation();
         setLightboxOpen(!lightboxOpen);
     }
 
@@ -26,54 +26,72 @@ const Service = (service) => {
         e.stopPropagation();
         console.log(e)
     }
-    
+
     const serviceName = service.name.length > 20 ? service.name.slice(0, 20) + '...' : service.name;
     const serviceText = service.text.length > 110 ? service.text.slice(0, 110) + '...' : service.text;
-    
-        
+
     const serviceRef = useRef(null);
     const isInView = useInView(serviceRef, { once: true });
     const initialAnimation = { scale: .25, rotate: -45, opacity: 0.33 };
-    const animateAnimation = { rotate: 0, scale: 1, opacity: 1 };
+    const animateAnimation = { scale: 1, rotate: 0, opacity: 1 };
     const transitionAnimation = {
         type: "spring",
-        stiffness: 260,
+        stiffness: 250,
         damping: 20
+    };
+    const lightboxVariants = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+        exit: { opacity: 0 }
     };
 
     return (
         <motion.div 
-        initial={isInView ? animateAnimation : initialAnimation}
-        animate={isInView ? animateAnimation : initialAnimation}
-        transition={transitionAnimation}
-        ref={serviceRef}
-        className="servicePreviewBox p-4 h-40vh w-[24vw] bg-[var(--third-color)] rounded-2xl hover:bg-[var(--main-color)] hover:text-[var(--third-color)] text-[var(--main-color)]" 
-        onClick={toggleLightbox}
+            initial={isInView ? animateAnimation : initialAnimation}
+            animate={isInView ? animateAnimation : initialAnimation}
+            transition={transitionAnimation}      
+            whileHover={!lightboxOpen ? { backgroundColor: "var(--main-color)", color: "var(--third-color)", scale: 1.025 } : {}}
+            whileTap={!lightboxOpen ? { scale: .95 } : {}}
+            ref={serviceRef}
+            className="servicePreviewBox p-4 min-h-40vh w-[24vw] bg-[var(--third-color)] rounded-2xl text-[var(--main-color)]" 
+            onClick={(e) => toggleLightbox(e)}
         >
             <h3 className="font-semibold text-3xl pr-4">{serviceName}</h3>
             <p className="text-gray-500 py-2 text-lg font-normal">{serviceText}</p>
             {service.hasImage ? handleShowIcon(serviceName) : <div className="h-[10vh]"></div>}
 
             {lightboxOpen && (
-                <div className="fixed z-40 inset-0 flex-col bg-black bg-opacity-90 w-100 h-100 px-[10vw] py-[10vh]">   
-                    <div className="p-4 rounded-md bg-[var(--main-color)] h-full" onClick={preventToggleLightbox}> 
+                <motion.div 
+                    initial="hidden"
+                    animate="visible"
+                    exit="exit"
+                    variants={lightboxVariants}
+                    transition={{ duration: 0.2, delay: 0.4, bounceStiffness: 300, clamp: 20}}
+                    className="fixed z-40 inset-0 flex-col bg-black bg-opacity-90 w-100 h-100 px-[10vw] py-[10vh]"
+                    onClick={toggleLightbox}
+                >
+                    <motion.div 
+                        initial={{ opacity: 1, y: "-100vh", scale: 0}}
+                        animate={{ opacity: 1, y: "0vh", scale: 1 }}
+                        exit={{ opacity: 0, y: "-100vh", scale: 0.9 }}
+                        className="p-4 rounded-md bg-[var(--main-color)] h-full"
+                        onClick={preventToggleLightbox}
+                    > 
                         {service.icon ? handleShowIcon(service.name) : <div className=""></div>}    
                         <h3 className="font-semibold text-3xl pr-4 text-[var(--onSurface-color)] text-center">{service.name}</h3>
                         <p className="text-gray-300 py-2 text-lg font-normal">{service.text}</p>
 
-                        <button
+                        <motion.button
                             onClick={toggleLightbox}
-                            className="absolute top-2 right-2 m-4 mx-[10vw] my-[10vh] text-white text-5xl cursor-pointer hover:text-gray-300 transition-colors"
+                            className="absolute top-2 right-2 m-4 text-white text-5xl cursor-pointer hover:text-gray-300 transition-colors"
                         >
                             <BsX/>
-                        </button>
-                        {service.link && <a className="absolute mx-[10vw] my-[10vh] bottom-4 left-4 underline hover:opacity-80" onClick={toggleLightbox} href={service.link}>{service.linkText}</a>}
-                    </div>
-                </div>
+                        </motion.button>
+                        {service.link && <a className="absolute bottom-4 left-4 underline hover:opacity-80 text-[var(--onMain-color)]" onClick={toggleLightbox} href={service.link}>{service.linkText}</a>}
+                    </motion.div>
+                </motion.div>
             )}
         </motion.div>
-        
-
     )
 }
 
