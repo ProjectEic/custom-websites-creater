6109afa9fa65da9df589e8572de8849d74ec1ae6
Luca Schirmer <acu1410l@gmail.com>
2024-05-01 19:35:18 +0200

    dynmafication of Impressum and Datenschutz
    update content of impressum => contained an error
    => also no visual effects for link to the datenschutzerklärung
    - update responsive

--- custom-website-creater/src/app/layout.js
+++ custom-website-creater/src/app/layout.js
@@ -9,16 +9,42 @@ import Admin from "./componetns/admin/admin";
 import NotFound from "./404";
 
 const inter = Inter({ subsets: ["latin"] });
+const title = "My Website"; // TODO: make it DYNAMIC
+const metaData = ""; // TODO: make it DYNAMIC
 
 export default function RootLayout({ children }) {
   return (
     <Router>
       <html>
+        <head>
+          <title>{title}</title>
+          <meta name="description" content={metaData} />
+        </head>
         <body className={inter.className}>
           <Routes> 
             <Route path="/" element={children} /> 
-            <Route path="/Datenschutz" element={<Datenschutzerklaerung />} /> 
-            <Route path="/Impressum" element={<Impressum />} /> 
+            <Route path="/Datenschutz" element={<Datenschutzerklaerung 
+              datenschutz={{
+                name: "Franz Ferdinand",
+                street: "Stephanstraße 5",
+                postCodeWithCity: "76133 Karlsruhe",
+                country: "Germany",
+                telephone: "+49 721 123456",
+                email: "lucaschi@t-online.de",
+              }}
+            />} /> 
+            <Route path="/Impressum" element={<Impressum 
+              impressum={
+                {
+                  name: "Franz Ferdinand",
+                  street: "Stephanstraße 5",
+                  postCodeWithCity: "76133 Karlsruhe",
+                  country: "Germany",
+                  telephone: "+49 721 123456",
+                  email: "lucaschi@t-online.de",
+                }
+              }
+            />} /> 
             <Route path="/admin" element={<Admin/>} />
             <Route path="*" element={<NotFound/>} /> { /* TODO: CREATE proper 404-Page*/}
           </Routes>
