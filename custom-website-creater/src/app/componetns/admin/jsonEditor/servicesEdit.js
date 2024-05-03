a4a8755f0944d8c76b46300d11476d67bb8191aa
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2024-05-03 16:51:06 +0200

    Update servicesEdit.js

--- custom-website-creater/src/app/componetns/admin/jsonEditor/servicesEdit.js
+++ custom-website-creater/src/app/componetns/admin/jsonEditor/servicesEdit.js
@@ -42,11 +42,6 @@ const ServicesEdit = ({jsonDict, title, setDictFunc, arg1}) => {
                     </div>
                 ))}
 
-                  <button 
-                        onClick={() => { delete jsonDict[key];  setDictFunc(jsonDict, arg1)}} 
-                        className="mt-2 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
-                            -
-                    </button>
             </div>
 
             {/* Add Button */}
